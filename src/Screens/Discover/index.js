7e60d7a42ab50b9a929eee3ba71e55494c197c8a
MohamedAmineNasri <mohamedamine.nasri@esprit.tn>
2024-03-22 02:51:01 +0100

    Header

--- src/Screens/Discover/index.js
+++ src/Screens/Discover/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, View } from 'react-native'
+import { StatusBar, Text, View } from 'react-native'
 import { Colors, Fonts, Images } from "Constants";
 import styled from "styled-components/native";
 import {McText, McImage} from 'Components';
@@ -7,7 +7,18 @@ const Discover = ( {
     params,
 }) => (
     <Container>
-        <McText size={24}>Discover Screenn</McText>
+        <StatusBar hidden={true}/>
+        {/* Header Section */}
+        <HeaderSection >
+            <McText semi size={30} style={{lineHeight:35}}>Discover</McText>
+            <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+                <McImage source={Images.Search} style={{marginRight:20}}/>
+                <McImage source={Images.Notification}/>
+                <View style={{width:10, height:10, borderRadius:10, backgroundColor:Colors.primary, alignItems:'center', justifyContent:'center',position:'relative', top:-5, right:5}}>
+                    <McText size={10} style={{fontFamily: 'SourceSansPro-Regular'}}>3</McText>
+                </View>
+            </View>
+        </HeaderSection >
     </Container>
 )
 
@@ -15,7 +26,14 @@ const Discover = ( {
 const Container = styled.SafeAreaView`
     flex: 1;
     background: ${(props) => Colors.background}
-    justify-content: center;
+
+` 
+
+const HeaderSection = styled.View`
+    height: 90px;
+    margin: 0px 16px;
+    flex-direction: row;
+    justify-content: space-between;
     align-items: center
 ` 
 export default Discover
