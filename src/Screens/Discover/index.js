a9d7228ac0557723e121049d422c85240a0d2a0d
MohamedAmineNasri <mohamedamine.nasri@esprit.tn>
2024-03-22 04:56:05 +0100

    Banner Section

--- src/Screens/Discover/index.js
+++ src/Screens/Discover/index.js
@@ -3,6 +3,7 @@ import { StatusBar, Text, View } from 'react-native'
 import { Colors, Fonts, Images } from "Constants";
 import styled from "styled-components/native";
 import {McText, McImage} from 'Components';
+import {LinearGradient} from 'expo-linear-gradient'
 const Discover = ( {
     params,
 }) => (
@@ -19,6 +20,56 @@ const Discover = ( {
                 </View>
             </View>
         </HeaderSection >
+        <BannerSection>
+            <LinearGradient 
+            colors={['#4C4478', '#0C0C69']}
+            start={{x:0, y:0}}
+            end={{x:1, y:0}}
+            style={{
+                height: 200,
+                borderRadius:1,
+                backgroundColor: 'green',
+                justifyContent:'space-between',
+                flexDirection: 'row',
+                position: 'absolute',
+                bottom: 0,
+                left: 0,
+                right: 0,
+            }}>
+                <View style={{
+                    width: 175,
+                    margin: 20,
+                    justifyContent: 'space-between'
+                }}>
+                    <View style={{
+                        width: 79,
+                        height: 23,
+                        borderRadius: 12,
+                        backgroundColor: Colors.white,
+                        flexDirection: 'row',
+                        justifyContent:'space-around',
+                        alignItems: 'center'
+                    }}>
+                        <McImage source={Images.Soccer}></McImage>
+                        <McText size={12} color="#181829" style={{
+                            fontFamily: 'Inter-SemiBold'
+                        }}>Football</McText>
+                    </View>
+                    <McText size={17} style={{
+                        fontFamily: 'Inter-SemiBold'
+                    }}>Zidane and Real Madrid: When it went wrong and what's next</McText>
+                    <McText size={12} style={{
+                        fontFamily: 'Inter-Regular'
+                    }}>Tommorw, 06.30 PM</McText>
+                </View>
+            </LinearGradient>
+            <View style={{
+                flexDirection: 'row',
+                justifyContent: 'flex-end'
+            }}>
+                <McImage source={require('Assets/images/Zidane.png')}/>
+            </View>
+        </BannerSection>
     </Container>
 )
 
@@ -27,6 +78,11 @@ const Container = styled.SafeAreaView`
     flex: 1;
     background: ${(props) => Colors.background}
 
+` 
+const BannerSection = styled.View`
+    height: 200px;
+    margin: 25px 16px 0px;
+    /*background: red*/
 ` 
 
 const HeaderSection = styled.View`
